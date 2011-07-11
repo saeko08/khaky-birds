7cb58ab9a4ad8841f2822c6cb86823d57de41722
ptrochim <ptrochim@gmail.com>
2011-07-11 19:40:46 +0000

    input management implementation

--- Engine/src/com/hypefoundry/engine/core/Audio.java
+++ Engine/src/com/hypefoundry/engine/core/Audio.java
@@ -7,11 +7,24 @@ package com.hypefoundry.engine.core;
  * @author paksas
  *
  */
-public interface Audio {
+public interface Audio 
+{
 	
-	public Music newMusic(String filename);
+	/**
+	 * Instantiate a streamed music asset.
+	 * 
+	 * @param filename		path to the asset relative to the root 'assets' dir 
+	 * @return
+	 */
+	public Music newMusic( String filename );
 	
-	public Sound newSound(String filename);
+	/**
+	 * Instantiate a sound asset.
+	 * 
+	 * @param filename		path to the asset relative to the root 'assets' dir 
+	 * @return
+	 */
+	public Sound newSound( String filename );
 	
 }
 
--- Engine/src/com/hypefoundry/engine/core/Music.java
+++ Engine/src/com/hypefoundry/engine/core/Music.java
@@ -6,14 +6,61 @@ package com.hypefoundry.engine.core;
  * @author paksas
  *
  */
-public interface Music {
+public interface Music 
+{
+	/**
+	 * Starts playing the music.
+	 */
 	public void play();
+	
+	/**
+	 * Stops playing the music.
+	 */
 	public void stop();
+	
+	/**
+	 * Pauses the music playback.
+	 */
 	public void pause();
-	public void setLooping(boolean looping);
-	public void setVolume(float volume);
+	
+	/**
+	 * Defines whether the music should play continuously, or stop
+	 * once the entire clip is played.
+	 * 
+	 * @param looping
+	 */
+	public void setLooping( boolean looping );
+	
+	/**
+	 * Sets the music volume.
+	 * 
+	 * @param volume		range 0..1
+	 */
+	public void setVolume( float volume );
+	
+	/**
+	 * Tells whether the music is playing at the moment.
+	 * 
+	 * @return is the music playing?
+	 */
 	public boolean isPlaying();
+	
+	/**
+	 * Tells whether the music is stopped at the moment.
+	 * 
+	 * @return is the music stopped?
+	 */
 	public boolean isStopped();
+	
+	/**
+	 * Tells if the music is set to played in a looped mode.
+	 * 
+	 * @return is the music looped?
+	 */
 	public boolean isLooping();
+	
+	/**
+	 * Disposes of the music resource, freeing up the memory it used.
+	 */
 	public void dispose();
 }

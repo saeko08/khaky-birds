5ad9cfecc04060f7bf11a53902a8b87752499f2c
ptrochim@gmail.com <ptrochim@gmail.com@7ed1ea5f-a858-b1f9-de54-c17b1c4397a8>
2011-07-11 21:16:47 +0000

    update manager

--- trunk/Engine/src/com/hypefoundry/engine/game/Game.java
+++ trunk/Engine/src/com/hypefoundry/engine/game/Game.java
@@ -11,12 +11,64 @@ import com.hypefoundry.engine.core.*;
  * @author paksas
  *
  */
-public interface Game {
+public interface Game 
+{
+	/**
+	 * Returns an instance of the input manager.
+	 * @return
+	 */
 	public Input getInput();
+	
+	/**
+	 * Returns an instance of the filesystem manager. 
+	 * @return
+	 */
 	public FileIO getFileIO();
+	
+	/**
+	 * Returns an instance of the graphics system.
+	 * @return
+	 */
 	public Graphics getGraphics();
+	
+	/**
+	 * Returns an instance of the sound system. 
+	 * @return
+	 */
 	public Audio getAudio();
-	public void setScreen(Screen screen);
+	
+	/**
+	 * Sets a new active screen.
+	 * @param screen
+	 */
+	public void setScreen( Screen screen );
+	
+	/**
+	 * Returns the currently running screen.
+	 * @return
+	 */
 	public Screen getCurrentScreen();
+	
+	/**
+	 * Returns the startup screen. 
+	 * 
+	 * Implement this in order to set your startup screen, and then enclose the entire 
+	 * screen management logics within the screens themselves.
+	 * @return
+	 */
 	public Screen getStartScreen();
+	
+	/**
+	 * Adds a new updatable object.
+	 * 
+	 * @param updatable
+	 */
+	public void addUpdatable( Updatable updatable );
+	
+	/**
+	 * Removes an updatable object.
+	 * 
+	 * @param updatable
+	 */
+	public void removeUpdatable( Updatable updatable );
 }

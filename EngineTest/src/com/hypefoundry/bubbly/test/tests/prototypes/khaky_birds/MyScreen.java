e7c2bd0820255d4aef333507366df3a5f28f0bac
Szczepankowski <Szczepankowski@gmail.com>
2011-07-13 22:04:52 +0000

    lukasz's tests

---
+++ EngineTest/src/com/hypefoundry/bubbly/test/tests/prototypes/khaky_birds/MyScreen.java
@@ -0,0 +1,50 @@
+package com.hypefoundry.bubbly.test.tests.prototypes.khaky_birds;
+
+import com.hypefoundry.engine.game.Game;
+import com.hypefoundry.engine.game.Screen;
+
+public class MyScreen extends Screen 
+{
+
+	public MyScreen(Game game) 
+	{
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float deltaTime) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void present(float deltaTime) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}

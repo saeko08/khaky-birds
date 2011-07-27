67af1d741e1da2e704edccd74c331a5353af4e66
ptrochim <ptrochim@gmail.com>
2011-07-27 05:18:55 +0000

    new renderer batch commint

--- EngineTest/src/com/hypefoundry/bubbly/test/tests/prototypes/khaky_birds/entities/pedestrian/PedestrianAI.java
+++ EngineTest/src/com/hypefoundry/bubbly/test/tests/prototypes/khaky_birds/entities/pedestrian/PedestrianAI.java
@@ -8,8 +8,8 @@ import android.util.Log;
 import com.hypefoundry.engine.controllers.EntityController;
 import com.hypefoundry.engine.game.Entity;
 import com.hypefoundry.engine.game.World;
-import com.hypefoundry.engine.util.BoundingBox;
-import com.hypefoundry.engine.util.Vector3;
+import com.hypefoundry.engine.math.BoundingBox;
+import com.hypefoundry.engine.math.Vector3;
 
 
 /**
@@ -52,11 +52,11 @@ public class PedestrianAI extends EntityController
 		//pedestrian stops moving for a while
 		if (m_pedestrian.isMoving == false)
 		{
-			m_wait+=deltaTime;
+			m_wait += deltaTime;
 			
 			Log.d( "wait", m_wait + ", ");
 			
-			if (m_wait > 1.2)
+			if (m_wait > 0.2)
 			{
 				m_pedestrian.isMoving = true;
 				m_wait = 0;

6f79d2f9771838c7a110b3bb899dbc692b918cd1
ptrochim <ptrochim@gmail.com>
2011-07-31 17:47:25 +0000

    physics view, spatial grid, openGL renderer

---
+++ Engine/src/com/hypefoundry/engine/impl/core/input/SingleTouchHandler.java
@@ -0,0 +1,146 @@
+package com.hypefoundry.engine.impl.core.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.hypefoundry.engine.util.Pool;
+import com.hypefoundry.engine.util.PoolObjectFactory;
+import com.hypefoundry.engine.core.Input.TouchEvent;
+
+/**
+ * A touch handler for Android systems < 2.0, that don't support multi-touch
+ * screens.
+ * 
+ * @author paksas
+ *
+ */
+public class SingleTouchHandler implements TouchHandler 
+{
+	boolean 			m_isTouched;
+	int	 				m_touchX;
+	int 				m_touchY;
+	Pool<TouchEvent> 	m_touchEventPool;
+	List<TouchEvent> 	m_touchEvents = new ArrayList<TouchEvent>();
+	List<TouchEvent> 	m_touchEventsBuffer = new ArrayList<TouchEvent>();
+	float 				m_scaleX;
+	float 				m_scaleY;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param view				view the touches of which we want to listen to
+	 * @param scaleX			X scale of the screen
+	 * @param scaleY			Y scale of the screen
+	 */
+	public SingleTouchHandler( View view, float scaleX, float scaleY ) 
+	{
+		// create the TouchEvents objects factory
+		PoolObjectFactory<TouchEvent> factory = new PoolObjectFactory<TouchEvent>() 
+		{
+			@Override
+			public TouchEvent createObject() 
+			{
+				return new TouchEvent();
+			}
+		};
+		// set the touch listener on the selected view
+		m_touchEventPool = new Pool<TouchEvent>( factory, 100 );
+		view.setOnTouchListener( this );
+		
+		m_scaleX = scaleX;
+		m_scaleY = scaleY;
+	}
+	
+	@Override
+	public boolean onTouch( View v, MotionEvent event ) 
+	{
+		// the events are received on the UI thread, and read on the main loop thread
+		// or hell knows where, so we need to keep the access to our members synchronized
+		synchronized( this ) 
+		{
+			TouchEvent touchEvent = m_touchEventPool.newObject();
+			
+			switch ( event.getAction() ) 
+			{
+			case MotionEvent.ACTION_DOWN:
+				touchEvent.type = TouchEvent.TOUCH_DOWN;
+				m_isTouched = true;
+			break;
+			
+			case MotionEvent.ACTION_MOVE:
+				touchEvent.type = TouchEvent.TOUCH_DRAGGED;
+				m_isTouched = true;
+			break;
+			
+			case MotionEvent.ACTION_CANCEL: 	// fallthrough
+			case MotionEvent.ACTION_UP:
+				touchEvent.type = TouchEvent.TOUCH_UP;
+				m_isTouched = false;
+			break;
+			}
+			
+			touchEvent.x = m_touchX = (int)( event.getX() * m_scaleX );
+			touchEvent.y = m_touchY = (int)( event.getY() * m_scaleY );
+			m_touchEventsBuffer.add( touchEvent );
+			
+			return true;
+		}
+	}
+
+	@Override
+	public boolean isTouchDown( int pointer ) 
+	{
+		synchronized( this ) 
+		{
+			if( pointer == 0 )
+			{
+				return m_isTouched;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	@Override
+	public int getTouchX( int pointer ) 
+	{
+		synchronized( this ) 
+		{
+			return m_touchX;
+		}
+	}
+
+	@Override
+	public int getTouchY( int pointer ) 
+	{
+		synchronized( this ) 
+		{
+			return m_touchY;
+		}
+	}
+
+	@Override
+	public List<TouchEvent> getTouchEvents() 
+	{
+		synchronized( this ) 
+		{
+			int len = m_touchEvents.size();
+			for( int i = 0; i < len; i++ )
+			{
+				m_touchEventPool.free( m_touchEvents.get( i ) );
+			}
+			m_touchEvents.clear();
+			
+			m_touchEvents.addAll( m_touchEventsBuffer );
+			m_touchEventsBuffer.clear();
+			
+			return m_touchEvents;
+		}
+	}
+
+}

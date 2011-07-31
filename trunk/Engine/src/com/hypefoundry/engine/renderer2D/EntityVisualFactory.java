4f7a471b7fba97c297590f37cfc94f5255f22422
ptrochim@gmail.com <ptrochim@gmail.com@7ed1ea5f-a858-b1f9-de54-c17b1c4397a8>
2011-07-31 17:49:22 +0000

    physics view, spatial grid, openGL renderer

---
+++ trunk/Engine/src/com/hypefoundry/engine/renderer2D/EntityVisualFactory.java
@@ -0,0 +1,22 @@
+package com.hypefoundry.engine.renderer2D;
+
+import com.hypefoundry.engine.game.Entity;
+import com.hypefoundry.engine.renderer2D.EntityVisual;
+import com.hypefoundry.engine.util.ObjectFactory;
+
+/**
+ * Creates an instance of a VisualEntity.
+ * 
+ * @author paksas
+ *
+ */
+public interface EntityVisualFactory extends ObjectFactory< Entity, EntityVisual >
+{
+	/**
+	 * The factory method.
+	 * 
+	 * @param parentEntity
+	 * @return
+	 */
+	EntityVisual instantiate( Entity parentEntity );
+}

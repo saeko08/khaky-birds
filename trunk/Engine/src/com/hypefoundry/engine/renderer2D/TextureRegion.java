a8fe1f053d8aa279828f5add9a7d228917622aa1
ptrochim <ptrochim@7ed1ea5f-a858-b1f9-de54-c17b1c4397a8>
2011-09-06 06:14:45 +0000

    making texture region a resource

--- trunk/Engine/src/com/hypefoundry/engine/renderer2D/TextureRegion.java
+++ trunk/Engine/src/com/hypefoundry/engine/renderer2D/TextureRegion.java
@@ -3,7 +3,13 @@
  */
 package com.hypefoundry.engine.renderer2D;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.hypefoundry.engine.core.Resource;
 import com.hypefoundry.engine.core.Texture;
+import com.hypefoundry.engine.util.serialization.DataLoader;
+import com.hypefoundry.engine.util.serialization.xml.XMLDataLoader;
 
 /**
  * A region in an atlas that specifies a texture.
@@ -11,12 +17,24 @@ import com.hypefoundry.engine.core.Texture;
  * @author paksas
  *
  */
-public class TextureRegion
+public class TextureRegion extends Resource
 {
-	public final float 			m_u1, m_v1;
-	public final float 			m_u2, m_v2;
+	public float 			m_u1, m_v1;
+	public float 			m_u2, m_v2;
 	
-	public final Texture 		m_texture;
+	public Texture 			m_texture;
+	
+	/**
+	 * Default constructor.
+	 */
+	public TextureRegion()
+	{
+		m_u1 = 0;
+		m_v1 = 0;
+		m_u2 = 0;
+		m_v2 = 0;	
+		m_texture = null;
+	}
 	
 	/**
 	 * Constructor.
@@ -45,5 +63,49 @@ public class TextureRegion
 		}
 		
 		m_texture = texture;
+	}
+	
+	@Override
+	public void load() 
+	{
+		if ( m_texture != null )
+		{
+			// texture is already loaded
+			return;
+		}
+		
+		InputStream stream = null;
+		try 
+		{
+			stream = m_game.getFileIO().readAsset( m_assetPath );
+		} 
+		catch ( IOException e ) 
+		{
+			throw new RuntimeException( e );
+		}
+		
+		// parse the animation data
+		DataLoader texNode = XMLDataLoader.parse( stream, "TextureRegion" );
+		if ( texNode != null )
+		{
+			String atlasName = texNode.getStringValue( "atlasName" );
+			m_texture = m_resMgr.getResource( Texture.class, atlasName );
+			
+			float x = texNode.getFloatValue( "x" );
+			float y = texNode.getFloatValue( "y" );
+			float width = texNode.getFloatValue( "w" );
+			float height = texNode.getFloatValue( "h" );
+			
+			m_u1 = x / m_texture.getWidth();
+			m_v1 = y / m_texture.getHeight();
+			m_u2 = m_u1 + width / m_texture.getWidth();
+			m_v2 = m_v1 + height / m_texture.getHeight();
+		}
+	}
+	
+	@Override
+	public void release() 
+	{
+		// nothing to do here
 	}
 }

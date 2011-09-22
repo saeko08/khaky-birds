618afe8a1a26c85fec49943739bac0d96f6b4078
ptrochim <ptrochim@7ed1ea5f-a858-b1f9-de54-c17b1c4397a8>
2011-09-22 17:22:13 +0000

    bug fix - animation events get skipped at low framerate

--- EngineTest/unittests/com/hypefoundry/engine/test/renderer2D/AnimationTest.java
+++ EngineTest/unittests/com/hypefoundry/engine/test/renderer2D/AnimationTest.java
@@ -125,15 +125,59 @@ public class AnimationTest extends AndroidTestCase
 		// the event is transmitted only once during a frame
 		// even if the frame is displayed many times 
 		listener.m_eventsCount = 0;
-		player.getTextureRegion( eventsReceiver, 0.3f );
+		player.getTextureRegion( eventsReceiver, 0.1f );
 		eventsReceiver.processEvents();
 		assertEquals( 0, listener.m_eventsCount );
 		
-		player.getTextureRegion( eventsReceiver, 0.4f );
+		player.getTextureRegion( eventsReceiver, 0.1f );
 		eventsReceiver.processEvents();
 		assertEquals( 0, listener.m_eventsCount );
 		
-		player.getTextureRegion( eventsReceiver, 0.6f );
+		player.getTextureRegion( eventsReceiver, 0.2f );
+		eventsReceiver.processEvents();
+		assertEquals( 1, listener.m_eventsCount );
+	}
+	
+	public void testAnimEventsFromSkippedFramesStillEmitted()
+	{
+		TextureRegion[] frames = {
+				new TextureRegion(),
+				new TextureRegion(),
+				new TextureRegion(),
+		};
+		EntityEvent[][] events = {
+				null,
+				{
+					new EventMock()
+				},
+				null,
+		};
+		
+		Animation animation = new Animation( 0.2f, true, frames, events );
+		
+		// setup the entity that will be receiving the events
+		Entity eventsReceiver = new EntityMock();
+		eventsReceiver.registerEvent( AnimEvent.class, new EventFactory< AnimEvent >() { @Override public AnimEvent createObject() { return new AnimEvent(); } } );
+		EventsListenerMock listener = new EventsListenerMock();
+		eventsReceiver.attachEventListener( listener );
+		
+		// start the animation
+		AnimationPlayer player = new AnimationPlayer();
+		int animIdx = player.addAnimation( animation );
+		player.select( animIdx );
+		
+		player.getTextureRegion( eventsReceiver, 0 );
+		eventsReceiver.processEvents();
+		assertEquals( 0, listener.m_eventsCount );
+		
+		// and now we're skipping the entire animation
+		player.getTextureRegion( eventsReceiver, 0.5f );
+		eventsReceiver.processEvents();
+		assertEquals( 1, listener.m_eventsCount );
+		
+		// it will also work when you skip a few loops of animation, however all events will be transmitted just once
+		listener.m_eventsCount = 0;
+		player.getTextureRegion( eventsReceiver, 12.0f );
 		eventsReceiver.processEvents();
 		assertEquals( 1, listener.m_eventsCount );
 	}

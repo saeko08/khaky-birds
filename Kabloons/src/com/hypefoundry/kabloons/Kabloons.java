182238d29a7e355ffe16d2662e6b6350d6093c17
ptrochim <ptrochim@7ed1ea5f-a858-b1f9-de54-c17b1c4397a8>
2012-04-07 19:56:34 +0000

    logo movie

--- Kabloons/src/com/hypefoundry/kabloons/Kabloons.java
+++ Kabloons/src/com/hypefoundry/kabloons/Kabloons.java
@@ -2,12 +2,35 @@ package com.hypefoundry.kabloons;
 
 
 import com.hypefoundry.engine.game.Screen;
+import com.hypefoundry.engine.game.ScreenFactory;
 import com.hypefoundry.engine.impl.game.GLGame;
+import com.hypefoundry.engine.movies.MovieScreen;
 import com.hypefoundry.kabloons.loadingScreen.GenericLoadingScreenFactory;
 
 
 public class Kabloons extends GLGame 
 {	
+	class MainMenuFactory implements ScreenFactory
+	{
+		GLGame		m_game;
+		
+		/**
+		 * Constructor.
+		 * 
+		 * @param game
+		 */
+		MainMenuFactory( GLGame game )
+		{
+			m_game = game;
+		}
+		
+		@Override
+		public Screen createScreen() 
+		{
+			return new MainMenu( m_game, MainMenu.MenuScreen.MS_Main );
+		}
+	}
+	
 	@Override
 	public void onResume()
 	{
@@ -20,7 +43,7 @@ public class Kabloons extends GLGame
 	@Override
 	public Screen getStartScreen()
 	{
-		return new MainMenu( this, MainMenu.MenuScreen.MS_Main );
+		return new MovieScreen( this, "movies/logo.mp4", new MainMenuFactory( this ) );
 	}
 }
 

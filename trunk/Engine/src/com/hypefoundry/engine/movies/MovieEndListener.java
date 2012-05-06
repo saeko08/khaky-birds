14692dd0785be8d354c5a60df571ea89dc83f0c3
ptrochim <ptrochim@7ed1ea5f-a858-b1f9-de54-c17b1c4397a8>
2012-05-06 21:03:57 +0000

    displaying movies as comics - for compatibility and accessibility reasons

---
+++ trunk/Engine/src/com/hypefoundry/engine/movies/MovieEndListener.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.hypefoundry.engine.movies;
+
+/**
+ * @author Paksas
+ *
+ */
+public interface MovieEndListener 
+{
+	/**
+	 * Called when the movie has finished.
+	 */
+	void onMovieEnded();
+}
